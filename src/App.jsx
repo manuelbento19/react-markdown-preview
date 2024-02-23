8420b15f4782d6e0a4f883601b08d5c7d87429d2
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-02-23 10:26:32 +0100

    feat: Install React-Markdown Package

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import ReactMarkdown from 'react-markdown';
 
 function App() {
+  const [text,setText] = useState("")
 
+  const onChange = ({target:{value}})=> setText(value); 
+  
   return (
     <main>
       <h1>React Markdown Previewer</h1>
+      <aside>
+        <textarea name="" id="" cols="80" rows="20" onChange={onChange}></textarea>
+      </aside>
+      <section>
+        <ReactMarkdown>{text}</ReactMarkdown>
+      </section>
     </main>
   )
 }
